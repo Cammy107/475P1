97681f7a882570e5ff43f86e67647dc436035199
handshaker86 <handshaker@sjtu.edu.cn>
2025-06-14 22:05:05 +0800

    test: apply visual attack on a series of encrypted input with increasing embedding ration

--- p1_visualattack.py
+++ p1_visualattack.py
@@ -12,7 +12,7 @@ def extract_lsb_image_all_channels(image_path, Name):
         lsb_array = img_array[:, :, i] & 1
         lsb_image = (lsb_array * 255).astype(np.uint8)
         lsb_img = Image.fromarray(lsb_image)
-        lsb_img.save(f"{Name}.png")
+        lsb_img.save(f"vis_atk_{Name}.png")
         lsb_img.show()
         print(f"done")
         lsb_images.append(lsb_img)
@@ -20,5 +20,8 @@ def extract_lsb_image_all_channels(image_path, Name):
     return lsb_images
 
 if __name__ == "__main__":
-    extract_lsb_image_all_channels("input.png", "in")
-    extract_lsb_image_all_channels("output.png", "out")
+    # extract_lsb_image_all_channels("input.png", "in")
+    # extract_lsb_image_all_channels("output.png", "out")
+    extract_lsb_image_all_channels("input.png", "input")
+    for ratio in [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9]:
+        extract_lsb_image_all_channels(f"encrypted_input_rt={ratio}.png", f"vis_atk_rt={ratio}")
